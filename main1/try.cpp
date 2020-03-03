4fb0d9a5b445ec2a3ab9ecb66f084d58c5d4a829
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-04 05:14:42 +0800

    Update try.cpp

--- main1/try.cpp
+++ main1/try.cpp
@@ -150,7 +150,8 @@ delay(10);  //sleep(0.01)=10ms
 */int main()
   {
  
-  auto int tgSN,tgEW,t1;
+  auto int tgSN,tgEW,t1;// t1为整体变量，子函数可以调用
+ 
   int tr,tg,ty; //timing
 
   if(WiringPiSetup() == -1) //initialize wiringpi store fail or not
