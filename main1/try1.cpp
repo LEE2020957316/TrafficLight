9707c1df8c66a21eab277bb8546203a683ea51e0
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-03 00:21:24 +0000

    Update try1.cpp

--- main1/try1.cpp
+++ main1/try1.cpp
@@ -8,32 +8,49 @@ do
 {
 t=t+0.01;
 sleep(0.01);
-    if(tgEW=tgEW-5 ||tgSN=tgSN-5)
+    if(tgEW=tgEW-5 ||tgSN=tgSN-5)// only check for 5 mins each time
   {
     break;
   }
 }while(digitalRead(12==1)&&digitalRead(18==1));// has input signals
     digitalWrite (23,1); //operate timer;// digitalwrite(int pin, int value)// if value != 0 == high)
-     Getnewt();
      t=0;
 }
 
- void Getnewt()
+ void GetnewtEW()
   if(t<=1)
-   tgEW=tgEW||tgSN=tgSN;//? 3 chooses
+   tgEW=tgEW;// 3 chooses
     
   }
     else if(t>=1 && t<=2)
     {
-      tgEW+=5 || tgSN+=5;
+      tgEW+=5; 
     }
        else if(t>=2)
   {
-   tgEW+=10 || tgSN+=10; 
+   tgEW+=10;
   }
 
-tgEW=(t<=1)? 15:15+(t-1)*10;// 2 chooses
-tgSN=(t<=1)? 15:15+(t-1)*10;
+tgEW=(t<=1)? 15:15+(t-1)*10;// 2 or more chooses
+
+ 
+void GetnewtSN()
+  if(t<=1)
+   tgSN=tgSN; // 3 chooses
+    
+  }
+    else if(t>=1 && t<=2)
+    {
+   tgSN+=5;
+    }
+       else if(t>=2)
+  {
+     tgSN+=10; 
+  }
+
+
+
+tgSN=(t<=1)? 15:15+(t-1)*10;// 2 or more chooses
 
 
 /*Struct Time
