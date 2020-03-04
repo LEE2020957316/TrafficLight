c3892359ecc152c4886339999b7403c69ca722df
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-04 16:36:19 +0000

    Update try.cpp

--- main1/try.cpp
+++ main1/try.cpp
@@ -6,8 +6,8 @@
 #define redSN 23 // Pin 16
 #define greenEW 25 // Pin 22
 #define greenSN 12 // Pin 32
-#define yellowEW 2// Pin 3
-#define yellowSN 3 // Pin 5
+#define yellowEW 2// Pin 13
+#define yellowSN 3 // Pin 15
 #define sider 5// Pin 29 BCM !!!
 #define sideg 6// Pin 31 BCM !!!
 #define extrasensor1 4//Pin 16
