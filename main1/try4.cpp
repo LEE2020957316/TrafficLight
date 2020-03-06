559a14b40c342cd64ee238d21dc2c693e7020c72
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-06 18:41:25 +0000

    Update try4.cpp

--- main1/try4.cpp
+++ main1/try4.cpp
@@ -1,1 +1,19 @@
+#include<WiringPi.h>
+#include<stdio.h>
+#include<iostream>
+#includ<theread>
 
+
+#define redEW 18 // Pin 12
+#define redSN 23 // Pin 16
+#define greenEW 25 // Pin 22
+#define greenSN 12 // Pin 32
+#define yellowEW 2// Pin 13
+#define yellowSN 3 // Pin 15
+#define sider 5// Pin 29 BCM !!!
+#define sideg 6// Pin 31 BCM !!!
+#define extrasensor1 4//Pin 16
+#define extrasensor2 5 //Pin 18
+#define button 11//Pin 11
+
+using namespace std;
