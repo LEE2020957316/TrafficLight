8b286cfa87f85812ea00479cfe74a504e46933a8
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-14 15:40:41 +0000

    Update sentnew.cpp

--- main1/sentnew.cpp
+++ main1/sentnew.cpp
@@ -1,16 +1,15 @@
 
  void GetnewtEW()
- 
+ {
+ tgEW=(t<=1)? 15:15+(t-1)*10;// 2 or more chooses
   t1=tgEW;// change t1 every time
  }
 
-/*tgEW=(t<=1)? 15:15+(t-1)*10;// 2 or more chooses
+
 
 
 */void GetnewtSN()
- 
-  t1=tgSN;// change t1 every time
- }
-
-/*tgSN=(t<=1)? 15:15+(t-1)*10;// 2 or more chooses
-
+{
+ tgSN=(t<=1)? 15:15+(t-1)*10;// 2 or more chooses
+ t1=tgSN;// change t1 every time
+}
