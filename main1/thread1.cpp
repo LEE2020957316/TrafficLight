e37b927e82a4b34a64509fc9db96f58cc4f00125
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-28 21:08:20 +0800

    Create thread1.cpp

---
+++ main1/thread1.cpp
@@ -0,0 +1,83 @@
+  {
+ 
+  auto int tgSN,tgEW,t1;// t1为整体变量，子函数可以调用
+  int tr,tg,ty; //timing
+
+   while true // intialize succeed
+  {
+   
+  redSNini();
+  redEWini();
+  for (tr=3,tr>0,tr--)
+  {
+  digitalWrite(1,1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1); // all red
+  }
+  
+  greenEWini();
+  redSNini();
+  SNsensorini(); //EW green, and SN sensortimer star 
+  for(tgEW=10,tgEW>0,tgEW--)
+{
+   digitalWrite(6, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1); //EW green, else red
+   sensortimer();
+}
+
+ yellowEWini();
+ //redSNini();
+for(tyEW=3,tyEW>0,tyEW--)
+ {
+ digitalWrite(2, 1);
+  //digitalWrite(4, 1);
+  digitalWrite(5, 1); //EW yellow, else red
+  }
+  
+  redEWini();
+  greenSNini();
+   EWsesorini(); // SN green, and EW sensortimer star 
+    for(tgSN=10,tgSN>0,tgSN--)
+  {
+  digitalWrite(1, 1);
+ digitalWrite(26, 1);
+  digitalWrite(5, 1); //SN green, else red;
+   }
+   
+  yelloeSNini();
+//redEWini();
+for(tySN=3,tySN>0,tySN--)
+ {
+  //digitalWrite(1, 1);
+  digitalWrite(3, 1);
+  digitalWrite(5, 1); //SN yellow, else red
+   sensortimer();
+ }
+    
+ void button
+     unique_lock<mutex>locker(b_mutex);
+     if(digitalRead(0) == 0)// 检测到低电平
+     {
+        delay(20); // 延时销抖, for machine button 
+        if(digitalRead(0) == 0)// 检测到低电平
+        { 
+     }
+   else 
+       {
+       digitalWrite(1, GPIO.LOW);// no signal was checked, no light respon
+       digitalWrite(4, GPIO.LOW);
+       digitalWrite(6, GPIO.LOW);
+        }
+ 
+  walk_light:
+   //digitalWrite(18, GPIO.HIGH); // all side greenlight lighting
+    for(tgside=10,tgside>0,tgside--)
+  {
+  digitalWrite(1, 1);
+  digitalWrite(4, 1);
+  digitalWrite(6, 1); //side green, else red
+    }
+ return 0;
+ }
+   
