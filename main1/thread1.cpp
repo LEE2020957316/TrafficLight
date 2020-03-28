65c22ff2e3d3c780385d7c8d36a69d534c0d12f5
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-28 22:41:59 +0800

    Update thread1.cpp

--- main1/thread1.cpp
+++ main1/thread1.cpp
@@ -1,7 +1,7 @@
  
 
 {
-	
+ public: 	
   auto int tgSN,tgEW,t1;// t1为整体变量，子函数可以调用
   int tr,tg,ty; //timing
 
@@ -25,18 +25,7 @@
    digitalWrite(6, 1);
   digitalWrite(4, 1);
   digitalWrite(5, 1); //EW green, else red
-   //sensortimer();
-}
-	   
-void Yellow()
-{
- yellowEWini();
-for(ty=3,ty>0,ty--)
- {
- digitalWrite(2, 1);
-  //digitalWrite(4, 1);
-  digitalWrite(5, 1); //EW yellow, else red
-	button_cond.notify_all ();//线程等待的实现
+   sensortimer();
   }
   
   redEWini();
@@ -49,21 +38,27 @@ for(ty=3,ty>0,ty--)
   digitalWrite(5, 1); //SN green, else red;
    }
    
-   //redEWini()
+   void Yellow()
+   {
 for (ty>=0,ty=3,ty--) // quan 
  {
   //digitalWrite(1, 1);
+ digitalWrite(2, 1);
   digitalWrite(3, 1);
-  digitalWrite(5, 1); //SN EW car all yellow.
+  digitalWrite(5, 1); //SN EW car all yellow, sides are red.
   unique_lock<mutex>locker(b_mutex);
 button_cond.notify_all ();//线程等待的实现
  }
      
-  void button {    // thread button
-   pinMode(0,INPUT); //引脚0为BUTTON输入模式
-    pullUpDnControl(0,PUD_UP); //设置0号引脚上拉,(设置成上拉输入，引脚上就加了一个上拉电阻，那么引脚就默认是高电平，当再去读取这个引脚的时候，
- //就可以检测到低电平了) 
-  while true  {
+ void button 
+ {        // thread button
+    PinMode(3,OUTPUT); 
+ pinMode(0,INPUT); //引脚0为BUTTON输入模式
+   digitalWrite(3, LOW);  
+  pullUpDnControl(0,PUD_UP); //设置0号引脚上拉,(设置成上拉输入，引脚上就加了一个上拉电阻，那么引脚就默认是高电平，当再去读取这个引脚的时候，
+ //就可以检测到低电平了)
+
+while true  {
   unique_lock<mutex>locker(b_mutex);
  button_cond.wait (unique_lock& locker, [this]{if (ty == 0);})
    return true; retuen false;
