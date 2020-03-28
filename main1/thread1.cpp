52105b1751a0a86dfa7b330b840c158b072c13ce
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-03-28 21:48:08 +0800

    Update thread1.cpp

--- main1/thread1.cpp
+++ main1/thread1.cpp
@@ -53,31 +53,41 @@ for(tySN=3,tySN>0,tySN--)
   digitalWrite(3, 1);
   digitalWrite(5, 1); //SN yellow, else red
    sensortimer();
+  cond.notify_all ();//线程等待的实现
  }
+     
+     
     
- void button
-     unique_lock<mutex>locker(b_mutex);
+ void button {
+   pinMode(11,INPUT); //引脚0为BUTTON输入模式
+    pullUpDnControl(11,PUD_UP); //设置0号引脚上拉,(设置成上拉输入，引脚上就加了一个上拉电阻，那么引脚就默认是高电平，当再去读取这个引脚的时候，
+ //就可以检测到低电平了) 
+   while true  {
+   unique_lock<mutex>locker(b_mutex);
+ button_cond.wait (unique_lock& locker, [this]{if (ty == 0);})
+   return true; retuen false;
+    
      if(digitalRead(0) == 0)// 检测到低电平
      {
-        delay(20); // 延时销抖, for machine button 
+        delay(20); // 延时销抖, for machine button
         if(digitalRead(0) == 0)// 检测到低电平
-        { 
-     }
-   else 
+        {  
+           for(tgside=10,tgside>0,tgside--)
+         {
+               digitalWrite(1, 1);
+               digitalWrite(4, 1);
+               digitalWrite(6, 1); //side green, else red
+              sensor.unlock();
+		            car.lock(); 
+          } 
+          else 
        {
-       digitalWrite(1, GPIO.LOW);// no signal was checked, no light respon
-       digitalWrite(4, GPIO.LOW);
-       digitalWrite(6, GPIO.LOW);
-        }
+       digitalWrite(1, 0);// no signal was checked, no light respon
+       digitalWrite(4, 0);
+       digitalWrite(6, 0);
+        sensor.unlock();
+		    car.lock();    
+       }
+   }
  
   walk_light:
-   //digitalWrite(18, GPIO.HIGH); // all side greenlight lighting
-    for(tgside=10,tgside>0,tgside--)
-  {
-  digitalWrite(1, 1);
-  digitalWrite(4, 1);
-  digitalWrite(6, 1); //side green, else red
-    }
- return 0;
- }
-   
