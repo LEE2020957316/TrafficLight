1f2044f4f2ca7405149612097fc56a92cf827f69
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 00:23:12 +0800

    Update CarLight.h

--- mian2/CarLight.h
+++ mian2/CarLight.h
@@ -53,19 +53,7 @@ digitalWrite(2, LOW);
 } 
     private:
   int tgSN;
-  int main()
-  {
- 
-  auto int tgSN,tgEW,t1;// t1为整体变量，子函数可以调用
-  int tr,tg,ty; //timing
 
-  if(WiringPiSetup() == -1) //initialize wiringpi store fail or not
- {
-  printf("you set up wiringpi failed"); //failed
-  return 1;
- }
-  
- 
    while true // intialize succeed
   {
    
