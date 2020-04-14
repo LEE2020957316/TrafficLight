b3b5e96c3974383397416c33f373009517ef96c4
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 00:20:35 +0800

    Update Button.h

--- mian2/Button.h
+++ mian2/Button.h
@@ -1,1 +1,25 @@
-
+class Button{
+  if(digitalRead(0) == 0)// 检测到低电平
+     {
+        delay(20); // 延时销抖, for machine button 
+        if(digitalRead(0) == 0)// 检测到低电平
+        { 
+ goto  walk_light;
+     }
+   else 
+       {
+       digitalWrite(1, LOW);// no signal was checked, no light respon
+       digitalWrite(4, LOW);
+       digitalWrite(6, LOW);
+        }
+ 
+  walk_light:
+   //digitalWrite(18, GPIO.HIGH); // all side greenlight lighting
+    for(tgside=10,tgside>0,tgside--)
+  {
+  digitalWrite(1, 1);
+  digitalWrite(4, 1);
+  digitalWrite(6, 1); //side green, else red
+    }
+ return 0;
+ }
