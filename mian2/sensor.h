2f38a414511a3a21a1e877cc5d49343a459ecc69
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-14 23:19:21 +0800

    Update sensor.h

--- mian2/sensor.h
+++ mian2/sensor.h
@@ -1,66 +1,64 @@
 class SensorES{
   public: 
-  SensorES(int t=0){//初始化
+  SensorES(int t=0){
+     t=0;//初始化
     PinMode(23,OUTPUT);
+     digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
      }// pinmode (int pin, int mode), computer control it by 23
     public:
   //void Counter()
-  void GetT(CarLight & Tobj)
-    do
+  void GetT(CarLight & Tobj, t1=0;)
+    t1=Tobj.GettgEW();
+    if(Tobj.CounttgEW()-5=0 ||Tobj.CounttgSN()-5=0)// counterv t can not exceed 1 minutes?
+  {
+   do
     {
-t=t+0.01;
-delay(10);  //sleep(0.01)=10ms 
-  if(tgEW=t1-5 ||tgSN=t1-5)// counterv t can not exceed 1 minutes?
-  {
-   t=0;
-   digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
-  }
-}
-while(digitalRead(12)==1);// has input signals
-}
-    virtual int outputT()
-    { reture t1; }
+   t=t+0.01;
+   ds delay(10);  //sleep(0.01)=10ms
+  }while(digitalRead(12)==(1));// has input signals
+    }
+  virtual int outputT()
+    { reture t; }
   private: 
-  int t1;
+  int t;
 
-
-
-void sensortimer()
-{
-PinMode(23,OUTPUT);// pinmode (int pin, int mode), computer control it by 23
-int t1,t=0;
-do
-{
-t=t+0.01;
-delay(10);  //sleep(0.01)=10ms 
-    if(tgEW=t1-5 ||tgSN=t1-5)// only check for 5 seconds each time
-  {
-    break;
-  }
-}while(digitalRead(12==1)&&digitalRead(18==1));// has input signals
-    digitalWrite (23,1); //operate timer;// digitalwrite(int pin, int value)// if value != 0 == high)
-     t=0;
-}
 class SensorWN: punlic SensorES 
 {
-  public: SensorES( int a; int t2=0):
-  {//初始化
-    
-    public: 
-    virtual int outputT()
- void Getnewt(SensorES & Ts)
- {
-  if(Ts.outputT())
+ public: 
+  SensorES(int t=0){
+     t=0;//初始化
+    PinMode(23,OUTPUT);
+     digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
+     }// pinmode (int pin, int mode), computer control it by 23
+    public:
+  //void Counter()
+  void GetT(CarLight & Tobj, t1=0;)
+    t1=Tobj.GettgEW();
+    if(Tobj.CounttgEW()=t1-5 ||Tobj.CounttgSN=t1-5)// counterv t can not exceed 1 minutes?
   {
-   tgEW=tgEW;// 3 chooses
-  }
-    else if(t>=1 && t<=2)
+   do
     {
-      tgEW+=5; 
+   t=t+0.01;
+   ds delay(10);  //sleep(0.01)=10ms
+  }while(digitalRead(18==(1)));// has input signals
     }
-       else if(t>=2)
+  virtual int outputT()
+    { reture t; }
+  private: 
+  int t;
+  
+ void Getnewt(SensorES & Ts, CarLight & Tc)
+ {
+  if(Ts.outputT()<1)
   {
-   tgEW+=10;
+   Tc.Gettg()= Tc.Gettg();// 3 chooses
   }
-  t1=tgEW;// change t1 every time
+    else if(Ts.outputT()>=1 && Ts.outputT()<=2)
+    {
+      Tc.Gettg()+=5; 
+    }
+       else if(Ts.outputT()>=2)
+  {
+    Tc.Gettg()+=10;
+  }
  }
