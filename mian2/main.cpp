8ee59c8145e2ea1fc779a02b63755d0d5c14fd2b
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 00:23:43 +0800

    Update main.cpp

--- mian2/main.cpp
+++ mian2/main.cpp
@@ -1,1 +1,13 @@
+  int main()
+  {
+ 
+  auto int tgSN,tgEW,t1;// t1为整体变量，子函数可以调用
+  int tr,tg,ty; //timing
 
+  if(WiringPiSetup() == -1) //initialize wiringpi store fail or not
+ {
+  printf("you set up wiringpi failed"); //failed
+  return 1;
+ }
+  
+ 
