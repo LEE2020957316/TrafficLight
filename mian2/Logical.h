8ec419dc79aaaabd209afc9452950a44256b36e0
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 05:25:09 +0800

    Update Logical.h

--- mian2/Logical.h
+++ mian2/Logical.h
@@ -1,6 +1,16 @@
 class Logical{
   public:
-  void Gettg(){}//？！ 写在sensor 下面的函数 比较
+   int Gettg(SensorES & Obj1, SensorWN & Obj2)// 作比较 输入对象（同一个类的不同对象），(加锁)
+{
+  int tg=0;
+  //SensorES tes; SensorWN twn;
+  if(Newtg(Obj1)>=Newtg(Obj2))
+  { 
+    tg=Newtg(Obj1);
+  }
+  else {tg=Newtg(Obj2);}
+  return tg;
+ }
   private:
   lock
     CarLightEW C;
