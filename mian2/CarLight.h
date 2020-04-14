f66fa6fcc935fdde98ef024f69697c338ae75808
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 00:19:17 +0800

    Update CarLight.h

--- mian2/CarLight.h
+++ mian2/CarLight.h
@@ -53,7 +53,71 @@ digitalWrite(2, LOW);
 } 
     private:
   int tgSN;
+  int main()
+  {
+ 
+  auto int tgSN,tgEW,t1;// t1为整体变量，子函数可以调用
+  int tr,tg,ty; //timing
+
+  if(WiringPiSetup() == -1) //initialize wiringpi store fail or not
+ {
+  printf("you set up wiringpi failed"); //failed
+  return 1;
+ }
   
+ 
+   while true // intialize succeed
+  {
+   
+  redSNini();
+  redEWini();
+  for (tr=3,tr>0,tr--)
+  {
+  digitalWrite(1,1);
+  digitalWrite(4, 1);
+  digitalWrite(21, 1); // all red
+  }
+  
+  greenEWini();
+  redSNini();
+  SNsensorini(); //EW green, and SN sensortimer star 
+  for(tgEW=10,tgEW>0,tgEW--)
+{
+   digitalWrite(6, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1); //EW green, else red
+   sensortimer();
+}
+
+ yellowEWini();
+ //redSNini();
+for(tyEW=3,tyEW>0,tyEW--)
+ {
+ digitalWrite(2, 1);
+  //digitalWrite(4, 1);
+  //digitalWrite(5, 1); //EW yellow, else red
+  }
+  
+  redEWini();
+  greenSNini();
+   EWsesorini(); // SN green, and EW sensortimer star 
+    for(tgSN=10,tgSN>0,tgSN--)
+  {
+  digitalWrite(1, 1);
+ digitalWrite(26, 1);
+  digitalWrite(27, 1); //SN green, else red;
+   }
+   
+  yelloeSNini();
+//redEWini();
+for(tySN=3,tySN>0,tySN--)
+ {
+  //digitalWrite(1, 1);
+  digitalWrite(3, 1);
+  //digitalWrite(5, 1); //SN yellow, else red
+   sensortimer();
+ }
+    
   /*void SNsensorini()
  {
 PinMode(5,INPUT);
