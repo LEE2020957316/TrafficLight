8734aaca60fd063d1f3e827700d3c1f5c9873c00
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 04:17:29 +0800

    Update sensor.h

--- mian2/sensor.h
+++ mian2/sensor.h
@@ -3,7 +3,6 @@ class SensorES{
   SensorES(int t=0){
      t=0;//初始化
     PinMode(23,OUTPUT);
-     digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
      }// pinmode (int pin, int mode), computer control it by 23
     public:
   //void Counter()
@@ -15,10 +14,12 @@ class SensorES{
     {
    t=t+0.01;
    ds delay(10);  //sleep(0.01)=10ms
-  }while(digitalRead(12)==(1));// has input signals
+  }while(digitalRead(12)==(1));// has input signals 被遮挡
     }
   virtual int outputT()
-    { reture t; }
+    { 
+    digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
+    reture t; }
   private: 
   int t;
 
@@ -28,13 +29,12 @@ class SensorWN: punlic SensorES
   SensorES(int t0=0){
      t=0;//初始化
     PinMode(23,OUTPUT);
-     digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
      }// pinmode (int pin, int mode), computer control it by 23
     public:
   //void Counter()
   void GetT(CarLight & Tobj, t1=0;)
     t1=Tobj.GettgEW();
-    if(Tobj.CounttgEW()=t1-5 ||Tobj.CounttgSN=t1-5)// counterv t can not exceed 1 minutes?
+    if(Tobj.CounttgEW()=t1-5 ||Tobj.CounttgSN()=t1-5)// counterv t can not exceed 1 minutes?
   {
    do
     {
@@ -43,22 +43,37 @@ class SensorWN: punlic SensorES
   }while(digitalRead(18==(1)));// has input signals
     }
   virtual int outputT()
-    { reture t0; }
+    {  
+    digitalWrite (23,1);    //operate timer; digitalwrite(int pin, int value)// if value != 0 == high)
+    reture t0; 
+  }
   private: 
   int t0;
   
- void Getnewt(SensorES & Ts, CarLight & Tc)
+ int Newtg(SensorES & Ts)
  {
   if(Ts.outputT()<1)
   {
-   Tc.Gettg()= Tc.Gettg();// 3 chooses
+   return 10;//Tc.Gettg()= Tc.Gettg();// 3 chooses
   }
-    else if(Ts.outputT()>=1 && Ts.outputT()<=2)
+     else if(Ts.outputT()>=1 && Ts.outputT()<=2)
     {
-      Tc.Gettg()+=5; 
+      return 15//Tc.Gettg()+=5; 
     }
        else if(Ts.outputT()>=2)
   {
-    Tc.Gettg()+=10;
+    return 20;//Tc.Gettg()+=10;
   }
+ }// 以上为四个sensor线程需要的全部过程：输出时间并计算
+int Gettg(SensorES & Obj1, SensorWN & Obj2)// 作比较 输入对象（同一个类的不同对象），(加锁)
+{
+  int tg=0;
+  //SensorES tes; SensorWN twn;
+  if(Newtg(Obj1)>=Newtg(Obj2))
+  { 
+    tg=Newtg(Obj1);
+  }
+  else {tg=Newtg(Obj2);}
+  return tg;
  }
+ 
