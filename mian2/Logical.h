37c16568354f84351e8d7a1f1e08d56d3ff960e9
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 20:44:24 +0800

    Update Logical.h

--- mian2/Logical.h
+++ mian2/Logical.h
@@ -26,6 +26,23 @@ class Logical{
   else {tg=Newtg(Obj2);}
   return tg;
  }
+   void YellowLight(CarLightEW*YL)
+    { YL->CounterY();}
+    YellowLight(&CEW);
+     YellowLight(&CSN);
+    virtual void CounterGR()// EW绿灯亮 
+    {
+    greenEWini();
+  redSNini();
+  //SNsensorini();
+   for(tgEW=tg,tgEW>0,tgEW--)//EW green, and SN sensortimer star 
+  {
+   digitalWrite(6, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1); //EW green, else red
+     tg--;
+   //sensortimer();
+  }
   void sensor(){}
   void carlight(){}
   void walklight(){}
@@ -84,6 +101,132 @@ class SensorWN: punlic SensorES
   private: 
   int t2;
   int t1;
+      
+      
+  class CarLightEW
+{
+public: 
+  CarLightEW(int tgEW=0, int tyEW=3)
+  { 
+    redEWini();
+    greenEWini();
+    yellowEWini();
+    this->tgEW= tgEW;
+    this->tg= tg;
+   }
+ public:
+  void redEWini()
+{
+PinMode(1,OUTPUT);
+digitalWrite(1, LOW);
+}
+
+void greenEWini()
+{
+PinMode(6,OUTPUT);
+digitalWrite(6, LOW);
+}
+
+void yellowEWini()
+{
+PinMode(3,OUTPUT);
+digitalWrite(3, LOW);
+}
+ void GNewtg(Logical & logical, SensorES & Obj1, SensorWN & Obj2)//时间比较后所得
+    {
+    tg=logical.Gettg(Obj1,Obj2);
+    }
+  
+  virtual void CounterGR()// EW绿灯亮 
+    {
+    greenEWini();
+  redSNini();
+  //SNsensorini();
+   for(tgEW=tg,tgEW>0,tgEW--)//EW green, and SN sensortimer star 
+{
+   digitalWrite(6, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1); //EW green, else red
+     tg--;
+   //sensortimer();
+}
+ virtual void CounterY() 
+ {
+ yellowEWini();
+ //redSNini();
+for(tyEW=3,tyEW>0,tyEW--)
+ {
+ digitalWrite(2, 1);
+  //digitalWrite(4, 1);
+  //digitalWrite(5, 1); //EW yellow, else red(不用）
+  }
+  public: 
+  static int tg;
+  private:
+  int tgEW;
+   int tyEW;
+  }
+  int CarLightEW::tg=0;//tg initialize.
+
+  class CarLightSN: public CarLightEW
+ {
+     CarLightSN(int tgEW=0, int tyEW=3, int tySN=3, int tgSN=0 ): CarLightEW(tgEW, tyEW)
+     { 
+      redSNini();
+      greenSNini();
+      yellowSNini();
+       this->tgSN= tgSN;
+       this->tySN= tySN;
+     }
+   void redSNini()
+{
+PinMode(4,OUTPUT);
+digitalWrite(4, LOW);
+}
+   void greenSNini()
+{
+PinMode(26,OUTPUT);
+digitalWrite(26, LOW);
+}
+void yellowSNini()
+{
+PinMode(2,OUTPUT);
+digitalWrite(2, LOW);
+} 
+ void GNewtg(Logical & logical, SensorES & Obj1, SensorWN & Obj2)//时间比较后所得
+    {
+   CarLightEW::tg=logical.Gettg(Obj1,Obj2);
+    }
+  virtual void CounterGR()// SN绿灯亮
+    {
+   redEWini();
+  greenSNini();
+   EWsesorini(); // SN green, and EW sensortimer star 
+     tgEW= CarLightEW::tg;
+    for(tgSN=10,tgSN>0,tgSN--)
+  {
+  digitalWrite(1, 1);
+ digitalWrite(26, 1);
+  digitalWrite(27, 1); //SN green, else red;
+      CarLightEW::tg--;
+   }
+
+  virtual void CounterY() //黄灯亮
+  {
+  yelloeSNini();
+//redEWini();
+for(tySN=3,tySN>0,tySN--)
+ {
+  //digitalWrite(1, 1);
+  digitalWrite(3, 1);
+  //digitalWrite(5, 1); //SN yellow, else red
+   sensortimer();
+ }
+  private:
+     int tgSN;
+     int tySN;
+    
+   
 /* 1. public: 
   SensorES(){
      t=0;//初始化
