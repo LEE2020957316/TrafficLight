972fad7494df57f5d29b4fea71b831a837a175ff
LEE2020957316 <60438997+LEE2020957316@users.noreply.github.com>
2020-04-15 22:12:11 +0800

    Update Button.h

--- mian2/Button.h
+++ mian2/Button.h
@@ -1,31 +1,46 @@
 class Button{
-  void Button()// check the button
-{
-  Button(){
+  public:
+  friend class WalkLight;
+  Button(){//构造函数
     pinMode(0,INPUT); //引脚0为BUTTON输入模式
   pullUpDnControl(0,PUD_UP); //设置0号引脚上拉,(设置成上拉输入，引脚上就加了一个上拉电阻，那么引脚就默认是高电平，当再去读取这个引脚的时候，
    }
+ int CheckB()
+  {
     if(digitalRead(0) == 0)// 检测到低电平
      {
         delay(20); // 延时销抖, for machine button 
         if(digitalRead(0) == 0)// 检测到低电平
-        { 
- goto  walk_light;
-     }
+        { return 1;}
    else 
-       {
-       digitalWrite(1, LOW);// no signal was checked, no light respon
-       digitalWrite(4, LOW);
-       digitalWrite(6, LOW);
-        }
- 
-  walk_light:
-   //digitalWrite(18, GPIO.HIGH); // all side greenlight lighting
-    for(tgside=10,tgside>0,tgside--)
+       { return 0;}
+       }
+     
+   class WalkLight{
+    private: Button b;
+     int tgside;
+     int tw;
+     public:
+     WalkLight(int tgside=10; tw=0)
+     { 
+       this->tgside= tgside;
+      this->tw=tw;
+     }
+   void WLighting()
+   { 
+     tw=b.CheckB();//digitalWrite(18, GPIO.HIGH); // all side greenlight lighting
+     if(tw=1){
+    for(tgside=10;tgside>0;tgside--)
   {
   digitalWrite(1, 1);
   digitalWrite(4, 1);
   digitalWrite(6, 1); //side green, else red
     }
- return 0;
- }
+     }
+     else(tw=0)
+      {
+digitalWrite(1, LOW);// no signal was checked, no light respon
+       digitalWrite(4, LOW);
+       digitalWrite(6, LOW);
+        }
+   }
